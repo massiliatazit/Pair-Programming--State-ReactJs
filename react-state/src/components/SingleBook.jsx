d8b511d60eb9d8f3f42621c54791d33cb17d1efc
massiliatazit <massiliatazit@users.noreply.github.com>
2020-11-18 18:08:05 +0100

    issue fixed

--- react-state/src/components/SingleBook.jsx
+++ react-state/src/components/SingleBook.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SingleBook.css";
 import { Card, Row } from "react-bootstrap";
 
 class SingleBook extends React.Component {
@@ -9,14 +10,16 @@ class SingleBook extends React.Component {
     if (this.state.select === false) {
       this.setState({ select: true });
     } else {
-      this.setState({ selected: false });
+      this.setState({ select: false });
     }
   };
   render() {
     return (
       <div>
         <Row className="align-item-center">
-          <Card onClick={() => this.selected()} style={{ width: "18rem" }}>
+          <Card onClick={() => this.selected()} style={{ width: "18rem" }} className={ this.state.select===true ? "selectedCard mx-auto my-3"
+            : "mx-auto my-3"
+        }>
             <Card.Img variant="top" src={this.props.booklist.img} />
             <Card.Body>
               <Card.Title>{this.props.booklist.title}</Card.Title>
